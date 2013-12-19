b493c7061f77451c45ec699224a61cf12a3fdf0f
guinardA <guinard.arnaud@gmail.com>
2013-12-19 23:44:20 +0100

    Ajout des pions par IA, vérification des pions insérer

--- PolyLib.c
+++ PolyLib.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+EColor couleur;
+int penalite=0;
+
 //Ensemble des fonctions communes a toutes les groupes
 void InitLibrary(char name[50])
 {
@@ -17,6 +20,64 @@ void StartMatch()
 void StartGame(const EColor color,EPiece boardInit[4][10])
 {
 	printf("Lancement d'une partie\n");
+	printf("Placement des pions sur le terrain\n");
+	
+	couleur = color;
+
+	//A modifier avec la strategie de IA
+	//BOMBE
+	boardInit[0][0] = EPbomb;
+	boardInit[0][1] = EPbomb;
+	boardInit[0][2] = EPbomb;
+	boardInit[0][3] = EPbomb;
+	boardInit[0][4] = EPbomb;
+	boardInit[0][5] = EPbomb;
+	//SPY
+	boardInit[0][6] = EPspy;
+	//SCOUT
+	boardInit[0][7] = EPscout;
+	boardInit[0][8] = EPscout;
+	boardInit[0][9] = EPscout;
+	boardInit[1][0] = EPscout;
+	boardInit[1][1] = EPscout;
+	boardInit[1][2] = EPscout;
+	boardInit[1][3] = EPscout;
+	boardInit[1][4] = EPscout;
+	//MINER
+	boardInit[1][5] = EPminer;
+	boardInit[1][6] = EPminer;
+	boardInit[1][7] = EPminer;
+	boardInit[1][8] = EPminer;
+	boardInit[1][9] = EPminer;
+	//SERGENT
+	boardInit[2][0] = EPsergeant;
+	boardInit[2][1] = EPsergeant;
+	boardInit[2][2] = EPsergeant;
+	boardInit[2][3] = EPsergeant;
+	//LIEUTENANT
+	boardInit[2][4] = EPlieutenant;
+	boardInit[2][5] = EPlieutenant;
+	boardInit[2][6] = EPlieutenant;
+	boardInit[2][7] = EPlieutenant;
+	//CAPITAINE
+	boardInit[2][8] = EPcaptain;
+	boardInit[2][9] = EPcaptain;
+	boardInit[3][0] = EPcaptain;
+	boardInit[3][1] = EPcaptain;
+	//MAJOR
+	boardInit[3][2] = EPmajor;
+	boardInit[3][3] = EPmajor;
+	boardInit[3][4] = EPmajor;
+	//COLONEL
+	boardInit[3][5] = EPcolonel;
+	boardInit[3][6] = EPcolonel;
+	//GENERAL
+	boardInit[3][7] = EPgeneral;
+	//MARSHAL
+	boardInit[3][8] = EPmarshal;
+	//FLAG
+	boardInit[3][9] = EPflag;
+		
 }
 
 void EndGame()
@@ -42,6 +103,8 @@ void AttackResult(SPos armyPos,EPiece armyPiece,SPos enemyPos,EPiece enemyPiece)
 void Penalty()
 {
 	printf("Penalty\n");
+	penalite++;
+	printf("Nombre de pénalité : %i\n",penalite);
 }
 
 //Ensemble des fonctions liées a IA
--- stratego.c
+++ stratego.c
@@ -70,23 +70,120 @@ SGameState gameState;//Plateau du jeu a dupliquer afin éviter qu'une libraire m
 
 SMove move;
 char name[50];
+int game, erreur=0;
+EColor couleurJ1;
 
 srand(time(NULL));
 int couleur;
 
+//==========ATTENTION : CODE 1 JOUEUR, MODIF EN 2 JOUEUR
+
 j1InitLibrary(name);
 j1StartMatch(); //DEBUT DU MATCH, on peut avoir plusieurs parties dans un match choix du joueur
 
-couleur = (int)rand()%2;
-if(couleur == 0){ //Si c'est égal a 0 alors la librairie 1 sera rouge et la librari 2 sera bleu sinon inverse
+do {
+
+	//DIFINISSION DE LA COULEUR DU JOUEUR ET PLACEMENT DES PIONS PAR LE JOUEURS
+	couleur = (int)rand()%2;
 	EPiece boardInit[4][10];
-	j1StartGame(ECred,boardInit);
-	//j2StartGame(ECblue,boardInit); //Lorsqu'on aura 2 joueur
-} else{
-	EPiece boardInit[4][10];
-	j1StartGame(ECblue,boardInit);
-	//j2StartGame(ECred,boardInit); //Lorsqu'on aura 2 joueur
-}
+	
+	if(couleur == 0){ //Si c'est égal a 0 alors la librairie 1 sera rouge et la librari 2 sera bleu sinon inverse
+		j1StartGame(ECred,boardInit);
+		couleurJ1 = ECred;
+		//j2StartGame(ECblue,boardInit); //Lorsqu'on aura 2 joueur
+
+	} else{
+		j1StartGame(ECblue,boardInit);
+		couleurJ1 = ECblue;
+		//j2StartGame(ECred,boardInit); //Lorsqu'on aura 2 joueur
+	}
+
+	//VERIFICATION DU NOMBRE DE JETONS
+	//AJOUTER UNE BOUCLE QUI REDEMANDE AU JOUEUR DE ENTRER C'EST PION
+	int Bomb=0, Spy=0, Scout=0, Miner=0, Sergeant=0, Lieutenant=0, Captain=0, Major=0, Colonel=0, General=0, Marshal=0, Flag=0;
+	int i,j;
+	for(i=0;i<4;i++){
+		for(j=0;j<10;j++){
+			if(boardInit[i][j] == EPbomb) Bomb++;
+			if(boardInit[i][j] == EPspy) Spy++;
+			if(boardInit[i][j] == EPscout) Scout++;
+			if(boardInit[i][j] == EPminer) Miner++;
+			if(boardInit[i][j] == EPsergeant) Sergeant++;
+			if(boardInit[i][j] == EPlieutenant) Lieutenant++;
+			if(boardInit[i][j] == EPcaptain) Captain++;
+			if(boardInit[i][j] == EPmajor) Major++;
+			if(boardInit[i][j] == EPcolonel) Colonel++;
+			if(boardInit[i][j] == EPgeneral) General++;
+			if(boardInit[i][j] == EPmarshal) Marshal++;
+			if(boardInit[i][j] == EPflag) Flag++;
+		}
+	}
+
+	if(Bomb != 6){
+		printf("ERREUR Bomb\n");
+		erreur++;
+		j1Penalty();
+	} else if(Spy != 1){
+		printf("ERREUR Spy\n");
+		erreur++;
+		j1Penalty();
+	} else if(Scout != 8){
+		printf("ERREUR Scout\n");
+		erreur++;
+		j1Penalty();
+	} else if(Miner != 5){
+		printf("ERREUR Miner\n");
+		erreur++;
+		j1Penalty();
+	} else if(Sergeant != 4){
+		printf("ERREUR Sergeant\n");
+		erreur++;
+		j1Penalty();
+	} else if(Lieutenant != 4){
+		printf("ERREUR Lieutenant\n");
+		erreur++;
+		j1Penalty();
+	} else if(Captain != 4){
+		printf("ERREUR Captain\n");
+		erreur++;
+		j1Penalty();
+	} else if(Major != 3){
+		printf("ERREUR Major\n");
+		erreur++;
+		j1Penalty();
+	} else if(Colonel != 2){
+		printf("ERREUR Colonel\n");
+		erreur++;
+		j1Penalty();
+	} else if(General != 1){
+		printf("ERREUR General\n");
+		erreur++;
+		j1Penalty();
+	} else if(Marshal != 1){
+		printf("ERREUR Marshal\n");
+		erreur++;
+		j1Penalty();
+	}else if(Flag != 1){
+		printf("ERREUR Flag\n");
+		erreur++;
+		j1Penalty();
+	}
+
+	//ENREGISTRE LES PIONS DU JOUEUR1 SUR L'ETAT DE JEU COMPLET (A NE JAMAIS TRANSMETTRE A UNE IA)
+	for(i=0;i<4;i++){
+		for(j=0;j<10;j++){
+			SBox box;
+			box.content = couleurJ1;
+			box.piece = boardInit[i][j];
+			gameState.board[i][j] = box;
+		}
+	}
+	
+	printf("Voulez vous refaire une partie ? 1:oui, 0:non = ");
+	scanf ("%i",&game);
+	
+}while(game==1);
+	
 
 dlclose(j1Lib);
 return(1);
