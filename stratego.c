ea5d510ce99259c5d2f82616c0c609a03d361543
guinardA <guinard.arnaud@gmail.com>
2013-12-23 16:13:40 +0100

    Commencement de la factorisation du code en fonction

--- stratego.c
+++ stratego.c
@@ -4,6 +4,11 @@
 #include <time.h>
 #include "PolyLib.h"
 
+void initialisationContexteJeu(SGameState *gameState);
+int verificationNombrePiece(EPiece boardInit[4][10]);
+void enregistrePion(EPiece boardInit[4][10], SGameState *gameState, EColor color);
+SGameState duplicationDuContexteDeJeu(SGameState gameState, EColor color);
+
 int main(int argc, const char * argv[]){
 
 //=========================Gestion ouverture de la librarie et des méthodes liés
@@ -73,6 +78,7 @@ char name[50];
 int game, erreur=0;
 EColor couleurJ1;
 SBox box, boxStart, boxEnd;
+EPiece boardInit[4][10];
 
 srand(time(NULL));
 int couleur,i,j, pion_erreur;
@@ -84,38 +90,13 @@ j1StartMatch(); //DEBUT DU MATCH, on peut avoir plusieurs parties dans un match 
 
 do {
 	
-	if(pion_erreur == 1){
-		printf("Une erreur c'est produit dans le nombre des différents pions possible\n");
-	}
-	pion_erreur = 0;
-	
-	
 	//INITIALISATION DU CONTEXTE DE JEU
-	for(i=0;i<10;i++){
-		for(j=0;j<10;j++){
-			//PLACEMENT DES LACS
-			if((i==4 && j==2) || (i==4 && j==3) || (i==4 && j==6) || (i==4 && j==7) ||(i==5 && j==2) || (i==5 && j==3) || (i==5 && j==6) || (i==5 && j==7)){
-				box.content = EClake;
-				box.piece = EPnone;
-				gameState.board[i][j] = box;
-			}
-			else{
-				box.content = ECnone;
-				box.piece = EPnone;
-				gameState.board[i][j] = box;
-			}
-		}
-	}
-	for(i=0;i<11;i++){
-		gameState.redOut[i] = 0;
-		gameState.blueOut[i] = 0;
-	}
+	initialisationContexteJeu(&gameState);
 
 	//DIFINISSION DE LA COULEUR DU JOUEUR ET PLACEMENT DES PIONS PAR LE JOUEURS
 	couleur = (int)rand()%2;
-	EPiece boardInit[4][10];
 
-	//Boucle car on replace les pions si on a pas le bon nombre de pions
+	//ENREGISTREMENT ET VERIFICATION DES PIONS DU JOUEUR 1
 	do{
 		if(couleur == 0){ //Si c'est égal a 0 alors la librairie 1 sera rouge et la librari 2 sera bleu sinon inverse
 			j1StartGame(ECred,boardInit);
@@ -127,117 +108,26 @@ do {
 			couleurJ1 = ECblue;
 			//j2StartGame(ECred,boardInit); //Lorsqu'on aura 2 joueur
 		}
-
-		//VERIFICATION DU NOMBRE DE JETONS
-		int Bomb=0, Spy=0, Scout=0, Miner=0, Sergeant=0, Lieutenant=0, Captain=0, Major=0, Colonel=0, General=0, Marshal=0, Flag=0;
-		for(i=0;i<4;i++){
-			for(j=0;j<10;j++){
-				if(boardInit[i][j] == EPbomb) Bomb++;
-				if(boardInit[i][j] == EPspy) Spy++;
-				if(boardInit[i][j] == EPscout) Scout++;
-				if(boardInit[i][j] == EPminer) Miner++;
-				if(boardInit[i][j] == EPsergeant) Sergeant++;
-				if(boardInit[i][j] == EPlieutenant) Lieutenant++;
-				if(boardInit[i][j] == EPcaptain) Captain++;
-				if(boardInit[i][j] == EPmajor) Major++;
-				if(boardInit[i][j] == EPcolonel) Colonel++;
-				if(boardInit[i][j] == EPgeneral) General++;
-				if(boardInit[i][j] == EPmarshal) Marshal++;
-				if(boardInit[i][j] == EPflag) Flag++;
-			}
-		}
-
-		if(Bomb != 6){
-			printf("ERREUR Bomb\n");
+		
+		pion_erreur =  verificationNombrePiece(boardInit);
+		//On envoie une erreur au joueur
+		if(pion_erreur == 1){
 			erreur++;
 			j1Penalty();
-			pion_erreur=1;
-		} else if(Spy != 1){
-			printf("ERREUR Spy\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Scout != 8){
-			printf("ERREUR Scout\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Miner != 5){
-			printf("ERREUR Miner\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Sergeant != 4){
-			printf("ERREUR Sergeant\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Lieutenant != 4){
-			printf("ERREUR Lieutenant\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Captain != 4){
-			printf("ERREUR Captain\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Major != 3){
-			printf("ERREUR Major\n");
-			erreur++;
-			j1Penalty();
-		} else if(Colonel != 2){
-			printf("ERREUR Colonel\n");
-			erreur++;
-			j1Penalty();
-		} else if(General != 1){
-			printf("ERREUR General\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
-		} else if(Marshal != 1){
-			printf("ERREUR Marshal\n");
-			erreur++;
-			j1Penalty();
-		}else if(Flag != 1){
-			printf("ERREUR Flag\n");
-			erreur++;
-			j1Penalty();
-			pion_erreur=1;
 		}
 	}while(pion_erreur == 1);
 
-	//ENREGISTRE LES PIONS DU JOUEUR1 SUR L'ETAT DE JEU COMPLET (A NE JAMAIS TRANSMETTRE A UNE IA)
-	for(i=0;i<4;i++){
-		for(j=0;j<10;j++){
-			box.content = couleurJ1;
-			box.piece = boardInit[i][j];
-			gameState.board[i][j] = box;
-		}
-	}
-	
+	//ENREGISTRE LES PIONS DU JOUEUR 1 SUR LE CONTEXTE DE JEU
+	enregistrePion(boardInit, &gameState, couleurJ1);
+		
 	//DEMANDE DE DEPLACEMENT D'UN PION
 	//SI LE JOUEUR 1 EST ROUGE ALORS IL DEPLACE CES PIONS EN PREMIER
 	if(couleurJ1 == ECred){
 		
 		//COPIE DU CONTEXTE DE JEU QU'AVEC LES PIONS DU JOUEUR1
-		for(i=0;i<10;i++){
-			for(j=0;j<10;j++){
-				if(gameState.board[i][j].content == ECblue){
-					box.content = ECnone;
-					box.piece = EPnone;
-					gameStateJ1.board[i][j] = box;
-				}
-				else{
-					gameStateJ1.board[i][j] = gameState.board[i][j];
-				}
-			}
-		}
-		for(i=0;i<11;i++){
-			gameStateJ1.redOut[i] = gameState.redOut[i];
-			gameStateJ1.blueOut[i] = gameState.blueOut[i];
-		}
+		gameStateJ1 = duplicationDuContexteDeJeu(gameState, ECblue);
 		move = j1NextMove(&gameStateJ1);
+		
 		if(move.start.line>=0 && move.start.line<=9 && move.start.col>=0 && move.start.col<=9){	
 			boxStart = gameState.board[move.start.line][move.start.col];
 			//VERIFICATION QUE LE PION SELECTIONNER CORRESPOND A UN PION DE LA BONNE COULEUR
@@ -317,4 +207,132 @@ do {
 
 dlclose(j1Lib);
 return(1);
+}
+void initialisationContexteJeu(SGameState *gameState){
+	
+	SBox box;
+	int i,j;
+	
+	for(i=0;i<10;i++){
+		for(j=0;j<10;j++){
+			
+			//PLACEMENT DES LACS
+			if((i==4 && j==2) || (i==4 && j==3) || (i==4 && j==6) || (i==4 && j==7) ||(i==5 && j==2) || (i==5 && j==3) || (i==5 && j==6) || (i==5 && j==7)){
+				box.content = EClake;
+				box.piece = EPnone;
+				gameState->board[i][j] = box;
+			} else{
+				box.content = ECnone;
+				box.piece = EPnone;
+				gameState->board[i][j] = box;
+			}
+		}
+	}
+	for(i=0;i<11;i++){
+		gameState->redOut[i] = 0;
+		gameState->blueOut[i] = 0;
+	}
+}
+
+int verificationNombrePiece(EPiece boardInit[4][10]){
+	
+	int Bomb=0, Spy=0, Scout=0, Miner=0, Sergeant=0, Lieutenant=0, Captain=0, Major=0, Colonel=0, General=0, Marshal=0, Flag=0, i, j;
+		
+	//VERIFICATION DU NOMBRE DE JETONS
+	for(i=0;i<4;i++){
+		for(j=0;j<10;j++){
+			if(boardInit[i][j] == EPbomb) Bomb++;
+			if(boardInit[i][j] == EPspy) Spy++;
+			if(boardInit[i][j] == EPscout) Scout++;
+			if(boardInit[i][j] == EPminer) Miner++;
+			if(boardInit[i][j] == EPsergeant) Sergeant++;
+			if(boardInit[i][j] == EPlieutenant) Lieutenant++;
+			if(boardInit[i][j] == EPcaptain) Captain++;
+			if(boardInit[i][j] == EPmajor) Major++;
+			if(boardInit[i][j] == EPcolonel) Colonel++;
+			if(boardInit[i][j] == EPgeneral) General++;
+			if(boardInit[i][j] == EPmarshal) Marshal++;
+			if(boardInit[i][j] == EPflag) Flag++;
+		}
+	}
+
+	if(Bomb != 6){
+		printf("ERREUR Bomb\n");
+		return 1;
+	} else if(Spy != 1){
+		printf("ERREUR Spy\n");
+		return 1;
+	} else if(Scout != 8){
+		printf("ERREUR Scout\n");
+		return 1;
+	} else if(Miner != 5){
+		printf("ERREUR Miner\n");
+		return 1;
+	} else if(Sergeant != 4){
+		printf("ERREUR Sergeant\n");
+		return 1;
+	} else if(Lieutenant != 4){
+		printf("ERREUR Lieutenant\n");
+		return 1;
+	} else if(Captain != 4){
+		printf("ERREUR Captain\n");
+		return 1;
+	} else if(Major != 3){
+		printf("ERREUR Major\n");
+		return 1;
+	} else if(Colonel != 2){
+		printf("ERREUR Colonel\n");
+		return 1;
+	} else if(General != 1){
+		printf("ERREUR General\n");
+		return 1;
+	} else if(Marshal != 1){
+		printf("ERREUR Marshal\n");
+		return 1;
+	}else if(Flag != 1){
+		printf("ERREUR Flag\n");
+		return 1;
+	}
+	return 0;
+}
+
+void enregistrePion(EPiece boardInit[4][10], SGameState *gameState, EColor color){
+	
+	SBox box;
+	int i,j;
+	
+	for(i=0;i<4;i++){
+		for(j=0;j<10;j++){
+			box.content = color;
+			box.piece = boardInit[i][j];
+			gameState->board[i][j] = box;
+		}
+	}
+}
+
+SGameState duplicationDuContexteDeJeu(SGameState gameState, EColor color){
+	
+	SGameState gameStatePerso;
+	SBox box;
+	int i,j;
+	
+	for(i=0;i<10;i++){
+		for(j=0;j<10;j++){
+			if(gameState.board[i][j].content == color){
+				box.content = color;
+				box.piece = EPnone;
+				gameStatePerso.board[i][j] = box;
+			}
+			else{
+				gameStatePerso.board[i][j] = gameState.board[i][j];
+			}
+		}
+	}
+	
+	for(i=0;i<11;i++){
+		gameStatePerso.redOut[i] = gameState.redOut[i];
+		gameStatePerso.blueOut[i] = gameState.blueOut[i];
+	}
+	
+	return gameStatePerso;
 }
